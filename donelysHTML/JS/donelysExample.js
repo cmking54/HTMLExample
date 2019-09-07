a19497c83786b0d1a13c29a41e625fc7052204b5
Donelys Familia <donelysprivate@gmail.com>
2019-09-07 17:07:27 -0400

    playing with js

--- donelysHTML/JS/donelysExample.js
+++ donelysHTML/JS/donelysExample.js
@@ -12,3 +12,41 @@ function getNewColor(newColor){
  coloring.style.color = newColor;
 
 }
+
+function addRow(){
+  const div = document.createElement('div');
+
+  div.className = 'row';
+
+
+  div.innerHTML = `
+
+  <div class='sidebar'>
+  <img src="imgs/chamomile.jpg" style="width:100%; opacity:0.9; height:250px">
+  <!--- ading the chosen target to sidebar  -->
+  </div>
+
+  <div class='content'>
+  <h2 class="headContent">Chamomile</h2>
+    <p>(Flower)</p>
+
+    <p>Considered by some to be a cure-all, chamomile is commonly used in the U.S.
+      as ananxiolytic and sedative for anxiety and relaxation.
+      It is used in Europe for wound healing and to reduce inflammation or swelling.
+      Few studies have looked at how well it works for any condition. Chamomile is used as a tea or applied as a compress. It is considered safe by the FDA. It may increase drowsiness caused by medicines or other herbs or supplements. Chamomile may interfere with the way the body uses some medicines, causing too high a level of the medicine in some people. As with any medicinal herb, talk with your healthcare provider before taking it.</p>
+
+  </div> <!--opeging content -->
+
+
+
+    <input type="button" value="-" onclick="removeRow(this)" />
+  `;
+
+  document.getElementById('InBigDivision').appendChild('div');
+}
+
+
+function removeRow(input) {
+  document.getElementById('InBigDivision').removeChild(input.parentNode);
+
+}

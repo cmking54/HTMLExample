27428a00c6c3c69e5a5a5ac020f73d63fc9416e4
Donelys Familia <donelysprivate@gmail.com>
2019-09-10 13:29:09 -0400

    playing with java Script

--- donelysHTML/JS/donelysExample.js
+++ donelysHTML/JS/donelysExample.js
@@ -13,40 +13,44 @@ function getNewColor(newColor){
 
 }
 
-function addRow(){
+function removeRow(input,secondInput) {
+    var firstElement = document.getElementById(input);
+    var secondElement = document.getElementById(secondInput)
+    firstElement.parentElement.removeChild(firstElement);
+    secondElement.parentElement.removeChild(secondElement);
+    console.log(document.getElementById('InBigDivision').childNodes.length);
+  }
+  
+
+
+
+
+function addContent(){
   const div = document.createElement('div');
-
-  div.className = 'row';
-
-
+  div.id = 'sidebar';
+  div.className = 'sidebar';
+  const div2 = document.createElement('contentID');
+  div2.id = 'content';
+  div2.className = 'content';
+  
   div.innerHTML = `
-
-  <div class='sidebar'>
   <img src="imgs/chamomile.jpg" style="width:100%; opacity:0.9; height:250px">
   <!--- ading the chosen target to sidebar  -->
   </div>
-
-  <div class='content'>
-  <h2 class="headContent">Chamomile</h2>
+`;
+  div2.innerHTML = `
+    <h2 class="headContent">Chamomile</h2>
     <p>(Flower)</p>
 
     <p>Considered by some to be a cure-all, chamomile is commonly used in the U.S.
       as ananxiolytic and sedative for anxiety and relaxation.
       It is used in Europe for wound healing and to reduce inflammation or swelling.
       Few studies have looked at how well it works for any condition. Chamomile is used as a tea or applied as a compress. It is considered safe by the FDA. It may increase drowsiness caused by medicines or other herbs or supplements. Chamomile may interfere with the way the body uses some medicines, causing too high a level of the medicine in some people. As with any medicinal herb, talk with your healthcare provider before taking it.</p>
-
-  </div> <!--opeging content -->
-
-
-
-    <input type="button" value="-" onclick="removeRow(this)" />
-  `;
-
-  document.getElementById('InBigDivision').appendChild('div');
-}
-
-
-function removeRow(input) {
-  document.getElementById('InBigDivision').removeChild(input.parentNode);
+    
+      <input type="button" value="-" onclick="removeRow('sidebar','content')" />
+      `
+    ;
+  document.getElementById('InBigDivision').appendChild(div);
+  document.getElementById('InBigDivision').appendChild(div2);
 
 }
